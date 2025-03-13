a520377245737fe397de3633e3491c212675b418
Baptiste Pillot <baptiste@pillot.fr>
2025-03-13 11:51:54 +0100

    Dynamic actions, template targets

--- src/login.ts
+++ src/login.ts
@@ -1,12 +1,16 @@
-import { Action }  from '@itrocks/action'
-import { Request } from '@itrocks/action-request'
-import { User }    from './user'
+import { Action }     from '@itrocks/action'
+import { getActions } from '@itrocks/action'
+import { Request }    from '@itrocks/action-request'
+import { routeOf }    from '@itrocks/route'
+import { User }       from './user'
 
 export class Login extends Action
 {
 
 	async html(request: Request<User>)
 	{
+		const route  = routeOf(this)
+		this.actions = getActions(request.type, route.slice(route.lastIndexOf('/') + 1))
 		return this.htmlTemplateResponse(new request.type, request, __dirname + '/login.html')
 	}
 
