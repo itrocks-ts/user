eb61b164e8ab7b6e3583fd60b6776b7407a31254
Baptiste Pillot <baptiste@pillot.fr>
2026-03-03 16:19:26 +0100

    Fix login view context after logout

--- src/logout.ts
+++ src/logout.ts
@@ -8,9 +8,10 @@ export class Logout<T extends User = User> extends Action<T>
 
 	async html(request: Request<T>)
 	{
-		delete request.request.session.user
-		request.request.session.destroy()
-		return new Login().html(request)
+		const session = request.request.session
+		delete session.user
+		session.destroy()
+		return new Login().html({...request, action: 'login'} as Request<User>)
 	}
 
 }
